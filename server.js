69be033780e29c91200f5a5226918c874b3923c5
Harshitha Rai <harshitharai@Harshithas-MacBook-Air.local>
2020-05-09 13:03:44 -0400

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
-const app = express();
+const connectDB = require('./config/db');
+const path = require('path');
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the ContactKeeper API' })
-);
+const app = express();
+//Connect DB
+
+connectDB();
+
+//Init Middleware
+
+app.use(express.json({ extended: false }));
 
 //define the routes
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set Static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
